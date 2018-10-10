64e9883b80d61585a4d28ce2f817ac8889f01095
Sangwoo Kim <slador@gmail.com>
2018-10-10 14:44:13 +0900

    Initial Commit

---
+++ ex__1010__ch00__breakPoint/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ex__1010__ch00__breakPoint
+//
+//  Created by Sangwoo Kim on 10/10/2018.
+//  Copyright © 2018 Sangwoo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var sum = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var count  = 0
+        for row in 5...10 {
+            count += 1
+            self.sum += row
+        }
+        
+        print("총 합은 \(self.sum), \(count)회 실행되었습니다.")
+        
+        
+    }
+
+    
+    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+
+
+}
+
